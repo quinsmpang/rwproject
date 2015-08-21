cf2113bbc2c79d3bb9446a2b33662310eb75891a
Gamesjiazhi <116024141>
2015-08-21 17:38:07 +0800

    1

--- undeadClient2D/src/Utils.lua
+++ undeadClient2D/src/Utils.lua
@@ -1,5 +1,43 @@
 -- 工具
 
+-- 获取时间格式
+function GetTimeFomat( totalsecond )
+	local day = math.floor( totalsecond / 86400 );
+	local hour = math.floor( totalsecond / 3600 );
+	local minitue = math.floor( totalsecond / 60 );
+	local second = math.floor( totalsecond ) % 60;
+	
+	local szTime = "";
+	if day > 0 then
+		szTime = szTime .. day .. "d";
+		if hour > 0 then
+			szTime = szTime .. " " .. hour .. "h";
+		end
+		return szTime;
+	end	
+
+	if hour > 0 then
+		szTime = szTime .. hour .. "h";
+		if minitue > 0 then
+			szTime = szTime .. " " .. minitue .. "m";
+		end
+		return szTime;
+	end
+
+	if minitue > 0 then
+		szTime = szTime .. minitue .. "m";
+		if second > 0 then
+			szTime = szTime .. " " .. second .. "s";
+		end
+		return szTime;
+	end	
+
+	if second > 0 then
+		szTime = szTime .. second .. "s";
+	end
+	return szTime;
+end
+
 -- 16进制颜色转换
 --[[function my.c3b( colorHex )
 	local _r = 
